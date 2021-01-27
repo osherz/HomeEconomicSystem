6be258a0c8dbf67f2c00f6ef8e9765f878d367aa
Osher Zakin <osherz92@gmail.com>
2021-01-27 19:44:41 +0200

    BasicGraph creation

---
+++ HomeEconomicSystem.BE/BasicGraph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEconomicSystem.BE
+{
+    public abstract class BasicGraph<T>
+    {
+        public int Id { get; set; }
+        public TimeType TimeType { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public GraphType GraphType { get; set; }
+        public virtual ICollection<T> Items { get; set; }
+    }
+}
