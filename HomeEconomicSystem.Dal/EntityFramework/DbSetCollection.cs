5d77c5941abb7ce97785795a4c322ada8ff9a6b2
Osher Zakin <osherz92@gmail.com>
2021-01-27 20:48:28 +0200

    Implement DAL interfaces with EntityFrameWork

---
+++ HomeEconomicSystem.Dal/EntityFramework/DbSetCollection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEconomicSystem.Dal.EntityFramework
+{
+    public class DbSetCollection<T> : IDbCollection<T>
+        where T : class
+    {
+        private DbSet<T> _dbSet;
+
+        public DbSetCollection(DbSet<T> dbSet)
+        {
+            _dbSet = dbSet;
+        }
+
+        public Expression Expression => (_dbSet as IQueryable).Expression;
+
+        public Type ElementType => (_dbSet as IQueryable).ElementType;
+
+        public IQueryProvider Provider => (_dbSet as IQueryable).Provider;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (_dbSet as IEnumerable<T>).GetEnumerator();
+        }
+
+        void IDbCollection<T>.Add(T item)
+        {
+            _dbSet.Add(item);
+        }
+
+        void IDbCollection<T>.AddRange(IEnumerable<T> items)
+        {
+            _dbSet.AddRange(items);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (_dbSet as IEnumerable).GetEnumerator();
+        }
+
+        void IDbCollection<T>.Remove(T item)
+        {
+            _dbSet.Remove(item);
+        }
+
+        void IDbCollection<T>.RemoveRange(IEnumerable<T> items)
+        {
+            _dbSet.RemoveRange(items);
+        }
+    }
+}
---
+++ HomeEconomicSystem.Dal/EntityFramework/HomeEconomicContext.cs
@@ -0,0 +1,61 @@
+﻿using HomeEconomicSystem.BE;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEconomicSystem.Dal.EntityFramework
+{
+    public class HomeEconomicContext : DbContext, IDb
+    {
+        internal HomeEconomicContext() : base()
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Store> Stores { get; set; }
+
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public DbSet<ProductTransaction> ProductTransactions { get; set; }
+
+        public DbSet<StoreGraph> StoreGraphs { get; set; }
+
+        public DbSet<ProductGraph> ProductGraphs { get; set; }
+
+        public DbSet<CategoryGraph> CategoryGraphs { get; set; }
+
+        public DbSet<TransactionsGraph> TransactionsGraphs { get; set; }
+
+
+        #region Implement IDb
+        IDbCollection<Product> IDb.Products => new DbSetCollection<Product>(Products);
+
+        IDbCollection<Category> IDb.Categories => new DbSetCollection<Category>(Categories);
+
+        IDbCollection<Store> IDb.Stores => new DbSetCollection<Store>(Stores);
+
+        IDbCollection<Transaction> IDb.Transactions => new DbSetCollection<Transaction>(Transactions);
+
+        IDbCollection<ProductTransaction> IDb.ProductTransactions => new DbSetCollection<ProductTransaction>(ProductTransactions);
+
+        IDbCollection<StoreGraph> IDb.StoreGraphs => new DbSetCollection<StoreGraph>(StoreGraphs);
+
+        IDbCollection<ProductGraph> IDb.ProductGraphs => new DbSetCollection<ProductGraph>(ProductGraphs);
+
+        IDbCollection<CategoryGraph> IDb.CategoryGraphs => new DbSetCollection<CategoryGraph>(CategoryGraphs);
+
+        IDbCollection<TransactionsGraph> IDb.TransactionsGraphs => new DbSetCollection<TransactionsGraph>(TransactionsGraphs);
+
+        void IDb.SaveChanges()
+        {
+            base.SaveChanges();
+        }
+        #endregion Implement IDb
+    }
+}
