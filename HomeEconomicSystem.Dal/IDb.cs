7cb941c6994e5e0f739cef4aa53c0d55a4108cc6
Osher Zakin <osherz92@gmail.com>
2021-01-27 20:47:20 +0200

    Create intefaces for DAL

---
+++ HomeEconomicSystem.Dal/IDb.cs
@@ -0,0 +1,25 @@
+﻿using HomeEconomicSystem.BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEconomicSystem.Dal
+{
+    public interface IDb: IDisposable
+    {
+        IDbCollection<Product> Products { get;}
+        IDbCollection<Category> Categories { get; }
+        IDbCollection<Store> Stores { get; }
+        IDbCollection<Transaction> Transactions { get; }
+        IDbCollection<ProductTransaction> ProductTransactions { get; }
+        IDbCollection<StoreGraph> StoreGraphs { get; }
+        IDbCollection<ProductGraph> ProductGraphs { get; }
+        IDbCollection<CategoryGraph> CategoryGraphs { get; }
+        IDbCollection<TransactionsGraph> TransactionsGraphs { get; }
+
+
+        void SaveChanges();
+    }
+}
---
+++ HomeEconomicSystem.Dal/IDbCollection.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HomeEconomicSystem.Dal
+{
+    public interface IDbCollection<T> : IQueryable<T>, IEnumerable<T>
+        where T: class
+    {
+        void Add(T item);
+        void AddRange(IEnumerable<T> items);
+        void Remove(T item);
+        void RemoveRange(IEnumerable<T> items);
+    }
+}
