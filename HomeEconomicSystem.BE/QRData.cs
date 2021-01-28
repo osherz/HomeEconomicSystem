85866b53e4b91dcb40f07d3399d93416068be97f
Tzur Eitan <zelp240@gmail.com>
2021-01-28 20:03:34 +0200

    Adding class for QRcode data named 'QRdata'

---
+++ HomeEconomicSystem.BE/QRData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEconomicSystem.BE
+{
+    public class QRData
+    {
+        public int Id { get; set; }
+        public string BarCode { get; set; }
+        public string Name { get; set; }
+        public float Amount { get; set; }
+        public float UnitPrice { get; set; }
+        public string StoreName{ get; set; }
+        public ProductTransaction ProductTransaction { get; set; }
+    }
+}
