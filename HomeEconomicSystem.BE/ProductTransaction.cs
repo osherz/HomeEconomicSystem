a8a42b4132b254a73bfec00659079cc903b16acb
Tzur Eitan <zelp240@gmail.com>
2021-01-28 20:03:34 +0200

    Adding class for QRcode data named 'QRdata'

--- HomeEconomicSystem.BE/ProductTransaction.cs
+++ HomeEconomicSystem.BE/ProductTransaction.cs
@@ -3,9 +3,14 @@
     public class ProductTransaction
     {
         public int Id { get; set; }
+        public float UnitPrice { get; set; }
+        /// <summary>
+        ///Amount of products.
+        /// </summary>
+        public float Amount { get; set; }
+        public virtual QRData QROriginalData { get; set; }
         public virtual Product Product { get; set; }
         public virtual Transaction Transaction { get; set; }
-        public float UnitPrice { get; set; }
-        public int Amount { get; set; }
+
     }
 }
---
+++ HomeEconomicSystem.BE/QRData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEconomicSystem.BE
+{
+    public class QRData
+    {
+        public int Id { get; set; }
+        public string BarCode { get; set; }
+        public string Name { get; set; }
+        public float Amount { get; set; }
+        public float UnitPrice { get; set; }
+        public string StoreName{ get; set; }
+        public bool IsNew { get => (ProductTransaction is null); }
+        public virtual ProductTransaction ProductTransaction { get; set; }
+    }
+}
