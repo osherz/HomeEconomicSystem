0b5c631f0d7d2eadafaefd1ffa1f6b1df4bb8b5c
Tzur Eitan <zelp240@gmail.com>
2021-02-01 17:56:09 +0200

    adding a test project for DAL

---
+++ HomeEconomicSystem.Dal.Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeEconomicSystem.Dal.GoogleDrive;
+
+namespace HomeEconomicSystem.Dal.Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            QRCodeFetcher qRCodeFetcher = new QRCodeFetcher();
+            IEnumerable<IQRcode> qRcode = qRCodeFetcher.GetQRCode();
+            foreach (var file in qRcode)
+            {
+                using (var img = System.IO.File.Create($"{file.FileName}.jpg"))
+                {
+                    img.Seek(0, SeekOrigin.Begin);
+                    var bytes = file.ImageStream;
+                    img.Write(bytes, 0, bytes.Length);           
+                }
+            }
+        }
+
+    }
+}
