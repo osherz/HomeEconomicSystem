bb63d47d92775aa0d9a05d9d03742a4d24e23133
Osher Zakin <osherz92@gmail.com>
2021-02-04 22:58:43 +0200

    Create DalFactory

---
+++ HomeEconomicSystem.Dal/DalFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEconomicSystem.Dal
+{
+    public class DalFactory
+    {
+        private IQRCodeFetcher _qRCodeFetcher;
+        private IDb _db;
+
+        public IQRCodeFetcher GetQRCodeFetcher() 
+        {
+            if(_qRCodeFetcher is null)
+            {
+                _qRCodeFetcher = new GoogleDrive.QRCodeFetcher();
+            }
+            return _qRCodeFetcher;
+        }
+        public IDb GetDb() 
+        {
+            if(_db is null)
+            {
+                _db = new EntityFramework.HomeEconomicContext();
+            }
+            return _db;
+        }
+    }
+}
