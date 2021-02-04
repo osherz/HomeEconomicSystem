a712bc8f7d082afb2e2d01d86f8149a99453776f
Osher Zakin <osherz92@gmail.com>
2021-02-04 21:44:08 +0200

    Add exception for fails in deletion files

---
+++ HomeEconomicSystem.Dal/DeleteFilesException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEconomicSystem.Dal
+{
+    /// <summary>
+    /// Occur when files deletion was failed.
+    /// </summary>
+    public class DeleteFilesException : Exception
+    {
+        public IEnumerable<string> FilesNotExists { get; private set; }
+        public IEnumerable<string> FailedToDelete { get; private set; }
+
+        public DeleteFilesException(IEnumerable<string> filesNotExists, IEnumerable<string> failedToDelete)
+        {
+            FilesNotExists = filesNotExists;
+            FailedToDelete = failedToDelete;
+        }
+    }
+}
