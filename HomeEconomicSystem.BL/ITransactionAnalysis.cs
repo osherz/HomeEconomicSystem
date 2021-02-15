1bb11b99a114c7975a575ecc1914df7b0af51c87
Osher Zakin <osherz92@gmail.com>
2021-02-15 19:08:57 +0200

    Add interface for transaction analysis

---
+++ HomeEconomicSystem.BL/ITransactionAnalysis.cs
@@ -0,0 +1,31 @@
+﻿using HomeEconomicSystem.BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEconomicSystem.BL
+{
+    public interface ITransactionAnalysis
+    {
+        /// <summary>
+        /// Return all new transactions that the user still not approve.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Transaction> GetNewTransactions();
+
+        /// <summary>
+        /// When active, start fetching purchases from remote drive in another threads,
+        /// When there is a new transaction call transactionDone callback.
+        /// </summary>
+        /// <param name="interval">Frequence of sample in seconds.</param>
+        /// <param name="transactionDone">Call when a new transaction found</param>
+        void StartFetchRemotePurchasesForTransactions(int interval, Action transactionDone);
+
+        /// <summary>
+        /// Stop StartFetchRemotePurchasesForTransactions.
+        /// </summary>
+        void StopFetchRemotePurchasesForTransactions();
+    }
+}
