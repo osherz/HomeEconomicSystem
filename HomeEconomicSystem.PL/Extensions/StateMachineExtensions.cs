3f2b5d20e5a188471c9d5d73ddba1249131302ed
Tzur Eitan <zelp240@gmail.com>
2021-02-18 19:54:34 +0200

    add documentation to create command in StateMachineExtensions

--- HomeEconomicSystem.PL/Extensions/StateMachineExtensions.cs
+++ HomeEconomicSystem.PL/Extensions/StateMachineExtensions.cs
@@ -11,6 +11,14 @@ namespace HomeEconomicSystem.PL.Extensions
 {
     public static class StateMachineExtensions
     {
+        /// <summary>
+        /// Create new connamd that will fire if he can.
+        /// </summary>
+        /// <typeparam name="TState"></typeparam>
+        /// <typeparam name="TTrigger"></typeparam>
+        /// <param name="stateMachine"></param>
+        /// <param name="trigger"></param>
+        /// <returns></returns>
         public static ICommand CreateCommand<TState, TTrigger>(this Stateless.StateMachine<TState, TTrigger> stateMachine, TTrigger trigger)
         {
             return new RelayCommand
