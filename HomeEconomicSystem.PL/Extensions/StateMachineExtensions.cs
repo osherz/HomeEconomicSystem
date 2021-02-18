d7e9706c127de531ee86028637d4b2152bd1b2ef
Osher Zakin <osherz92@gmail.com>
2021-02-18 19:18:07 +0200

    Instal stateless package and add CreateCommand extension to state-machine to be able to create command base on state

---
+++ HomeEconomicSystem.PL/Extensions/StateMachineExtensions.cs
@@ -0,0 +1,23 @@
+﻿using HomeEconomicSystem.PL.Command;
+using HomeEconomicSystem.PL.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HomeEconomicSystem.PL.Extensions
+{
+    public static class StateMachineExtensions
+    {
+        public static ICommand CreateCommand<TState, TTrigger>(this Stateless.StateMachine<TState, TTrigger> stateMachine, TTrigger trigger)
+        {
+            return new RelayCommand
+              (
+                () => stateMachine.Fire(trigger),
+                () => stateMachine.CanFire(trigger)
+              );
+        }
+    }
+}
