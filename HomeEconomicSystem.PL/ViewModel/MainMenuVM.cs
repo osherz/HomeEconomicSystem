1be53fb9ccadcaf6b9bccfe26fb0870ab148894a
Osher Zakin <osherz92@gmail.com>
2021-02-18 02:00:44 +0200

    Create MenuItem, MainMenu and it ViewModel

---
+++ HomeEconomicSystem.PL/ViewModel/MainMenuVM.cs
@@ -0,0 +1,26 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEconomicSystem.PL.ViewModel
+{
+    public class MainMenuVM
+    {
+        public ObservableCollection<MenuItem> MenuItems { get; private set; }
+
+        public MainMenuVM()
+        {
+            MenuItems = new ObservableCollection<MenuItem>
+            {
+                new MenuItem("בית", PackIconKind.Home, new System.Windows.Controls.UserControl()),
+                new MenuItem("ניתוח נתונים", PackIconKind.BarChart, new System.Windows.Controls.UserControl()),
+                new MenuItem("קטלוג מוצרים", PackIconKind.Newspaper, new System.Windows.Controls.UserControl()),
+                new MenuItem("היסטוריית קניות", PackIconKind.History, new System.Windows.Controls.UserControl()),
+            };
+        }
+    }
+}
---
+++ HomeEconomicSystem.PL/ViewModel/MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HomeEconomicSystem.PL.ViewModel
+{
+    /// <summary>
+    /// Represent view-model for menu item.
+    /// </summary>
+    public class MenuItem : ViewModelBase
+    {
+        public UserControl Content { get; private set; }
+        public string Text { get; private set; }
+        public PackIconKind IconKind { get; private set; }
+
+        public MenuItem(string text, PackIconKind iconKind, UserControl content)
+        {
+            Content = content;
+            Text = text;
+            IconKind = iconKind;
+        }
+    }
+}
