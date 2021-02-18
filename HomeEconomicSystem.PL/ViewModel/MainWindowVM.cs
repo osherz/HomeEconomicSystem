72e4498b3bb34900d241a6fdd9f9730500d10157
Osher Zakin <osherz92@gmail.com>
2021-02-18 20:31:08 +0200

    Create actions to the main states

--- HomeEconomicSystem.PL/ViewModel/MainWindowVM.cs
+++ HomeEconomicSystem.PL/ViewModel/MainWindowVM.cs
@@ -3,18 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace HomeEconomicSystem.PL.ViewModel
 {
-    public class MainWindowVM
+    public class MainWindowVM : NotifyPropertyChanged
     {
-        public MainMenuVM MainMenuVM { get; private set; }
         private Dictionary<States, Action> _stateActionDict;
         private StateMachine _stateMachine;
 
+        public MainMenuVM MainMenuVM { get; private set; }
+
+        private IPageDisplay _pageDisplay;
+        public IPageDisplay PageDisplay
+        {
+            get => _pageDisplay;
+            set => SetProperty(ref _pageDisplay, value);
+        }
+
         public MainWindowVM()
         {
-            _stateActionDict = new Dictionary<States, Action>();
+            UserControl userControl = new UserControl();
+            userControl.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+
+            IPageDisplay homePageDisplay = new PageDisplay(userControl, null);
+
+            _stateActionDict = new Dictionary<States, Action>
+            {
+                {States.Home, ()=>PageDisplay=homePageDisplay },
+                {States.DataAnalysis, ()=>PageDisplay=homePageDisplay },
+                {States.ProductCatalog, ()=>PageDisplay=homePageDisplay },
+                {States.TransactionHistory, ()=>PageDisplay=homePageDisplay }
+            };
             _stateMachine = new StateMachine(_stateActionDict);
 
             MainMenuVM = new MainMenuVM(_stateMachine);
