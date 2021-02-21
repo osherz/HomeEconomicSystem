dbdd762fb21398cc82850b0bb6d3d8462b5468ba
Tzur Eitan <zelp240@gmail.com>
2021-02-21 13:15:38 +0200

    add models for Produc and Categories

---
+++ HomeEconomicSystem.PL/Model/Categories.cs
@@ -0,0 +1,28 @@
+﻿using HomeEconomicSystem.BE;
+using HomeEconomicSystem.BL;
+using HomeEconomicSystem.PL.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEconomicSystem.PL.Model
+{
+    public class Categories
+    {
+        IDataManagement _dataMenegement;
+        public ObservableCollection<Category> CategoriesList { get; private set; }
+        public Categories()
+        {
+            _dataMenegement = new BL.BL().DataManagement;
+            CategoriesList = _dataMenegement.GetCategories().ToObservableCollection();
+        }
+
+        void Filter(string name = "")
+        {
+            CategoriesList = _dataMenegement.GetCategories(name).ToObservableCollection();
+        }
+    }
+}
---
+++ HomeEconomicSystem.PL/Model/Products.cs
@@ -0,0 +1,29 @@
+﻿using HomeEconomicSystem.BE;
+using HomeEconomicSystem.BL;
+using HomeEconomicSystem.PL.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEconomicSystem.PL.Model
+{
+    public class Products
+    {
+        IDataManagement _dataMenegement ;
+
+        public ObservableCollection<Product> ProductsList { get; private set; }
+        public Products()
+        {
+            _dataMenegement = new BL.BL().DataManagement;
+            ProductsList = _dataMenegement.GetProducts().ToObservableCollection();
+        }
+
+        void Filter(string name = "", params Category[] categories)
+        {
+            ProductsList = _dataMenegement.GetProducts(name, categories).ToObservableCollection();
+        }
+    }
+}
