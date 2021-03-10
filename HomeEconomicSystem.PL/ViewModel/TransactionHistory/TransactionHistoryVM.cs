2c3a4d90d817885a63eaa2a58dfec03201c97747
Osher Zakin <osherz92@gmail.com>
2021-03-10 21:17:19 +0200

    Start working on history

---
+++ HomeEconomicSystem.PL/ViewModel/TransactionHistory/TransactionHistoryVM.cs
@@ -0,0 +1,33 @@
+﻿using HomeEconomicSystem.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeEconomicSystem.PL.ViewModel.TransactionHistory
+{
+    public class TransactionHistoryVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private NotifyProperyChanged _notifyPropertyChanged;
+
+        public TransactionHistoryVM()
+        {
+            _notifyPropertyChanged = new NotifyProperyChanged(this, (property) => OnPropertyChanged(property));
+
+        }
+
+        private void SetProperty<T>(ref T property, T value, [CallerMemberName] string propertyName = "")
+        {
+            _notifyPropertyChanged.SetProperty(ref property, value, propertyName);
+        }
+
+        private void OnPropertyChanged(PropertyChangedEventArgs property)
+        {
+            PropertyChanged?.Invoke(this, property);
+        }
+    }
+}
